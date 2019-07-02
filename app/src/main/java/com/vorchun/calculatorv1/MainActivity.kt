e09ba43920f811c1c7c67df2bc9854167d7b9da8
Vor4un <Vorchun87-07@mail.ru>
2019-07-02 14:16:02 +0400

    refactoring activity main, show toast

--- app/src/main/java/com/vorchun/calculatorv1/MainActivity.kt
+++ app/src/main/java/com/vorchun/calculatorv1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vorchun.calculatorv1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,18 +13,23 @@ class MainActivity : AppCompatActivity() {
 
         var mFirstNumber = first_number.text
         var mSecondNumber = second_number.text
+        var mResult = result.text
 
         btn_sum.setOnClickListener {
-            val mResult = mFirstNumber.toString()?.toLong() + mSecondNumber.toString()?.toLong()
+            val mResult = mFirstNumber.toString().toLong() + mSecondNumber.toString()?.toLong()
+            Toast.makeText(this, mResult.toString(), Toast.LENGTH_LONG).show()
         }
         btn_sub.setOnClickListener {
-            val mResult = mFirstNumber.toString()?.toLong() - mSecondNumber.toString()?.toLong()
+            val mResult = mFirstNumber.toString().toLong() - mSecondNumber.toString()?.toLong()
+            Toast.makeText(this, mResult.toString(), Toast.LENGTH_LONG).show()
         }
         btn_div.setOnClickListener {
-            val mResult = mFirstNumber.toString()?.toLong() / mSecondNumber.toString()?.toLong()
+            val mResult = mFirstNumber.toString().toLong() / mSecondNumber.toString()?.toLong()
+            Toast.makeText(this, mResult.toString(), Toast.LENGTH_LONG).show()
         }
         btn_multi.setOnClickListener {
-            val mResult = mFirstNumber.toString()?.toLong() * mSecondNumber.toString()?.toLong()
+            val mResult = mFirstNumber.toString().toLong() * mSecondNumber.toString()?.toLong()
+            Toast.makeText(this, mResult.toString(), Toast.LENGTH_LONG).show()
         }
     }
 }
